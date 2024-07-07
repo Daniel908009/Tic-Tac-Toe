c108a6ec9ad3e5ba58fe65a31ae71ed9f11de5de
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-07-07 14:06:42 +0100

    All the different functions have been added, also some parts of the window are now resizable

--- main.py
+++ main.py
@@ -4,39 +4,101 @@ import random
 
 # function that reset the game
 def reset():
+    # resets the buttons to active state and empty text
     for i in range(size):
         for j in range(size):
             buttons[i][j]['text'] = ""
+            buttons[i][j]['state'] = "active"
 
-# function that checks if someone has won
-def check_for_win():
-    pass
+    # resets the global variables
+    global player_current, number_of_tiles
+    player_current = random.choice(players)
+    number_of_tiles = size*size
+    label['text'] = "Player "+ player_current +" turn"
+    
+    # changes the dimensions of the items in the window
+    label.config(font=("Helvetica", window.winfo_height()//size//4))
+    reset_button.config(width=int(window.winfo_width()/size/10), height=int(window.winfo_height()/size/10/4))
+    settings_button.config(width=int(window.winfo_width()/size/10), height=int(window.winfo_height()/size/10/4))
 
-# function that checks if the board is full, if yes than its a draw
-def check_if_tiles():
-    if number_of_tiles == 0:
-        label['text'] = "Draw"
-    else:
-        pass
+# function that stops the game(disables the buttons)
+def stop():
+    for i in range(size):
+        for j in range(size):
+            buttons[i][j]['state'] = "disabled"
+
+# function that checks if someone has won, checks all 8 directions, currently there is no way to change the number of tiles needed to win, will be added in the future
+def check_for_win(x, y):
+    # check for 8 directional wins
+    if buttons[x][y]['text'] == player_current:
+        # check for horizontal win
+        for i in range(size):
+            if buttons[x][i]['text'] != player_current:
+                break
+            if i == size-1:
+                label['text'] = "Player " + player_current + " wins"
+                return True
+        # check for vertical win
+        for i in range(size):
+            if buttons[i][y]['text'] != player_current:
+                break
+            if i == size-1:
+                label['text'] = "Player " + player_current + " wins"
+                return True
+        # check for diagonal win
+        if x == y:
+            for i in range(size):
+                if buttons[i][i]['text'] != player_current:
+                    break
+                if i == size-1:
+                    label['text'] = "Player " + player_current + " wins"
+                    return True
+        # check for anti diagonal win
+        if x + y == size - 1:
+            for i in range(size):
+                if buttons[i][(size-1)-i]['text'] != player_current:
+                    break
+                if i == size-1:
+                    label['text'] = "Player " + player_current + " wins"
+                    return True
+    return False
 
 # function that changes the ownership of a button/tile
 def change_ownership(x, y):
     global player_current, number_of_tiles
     if buttons[x][y]['text'] == "":
         buttons[x][y]['text'] = player_current
-        if player_current == "X":
-            player_current = "O"
+        if check_for_win(x, y):
+            stop()
         else:
-            player_current = "X"
-        number_of_tiles -= 1
+            if player_current == "X":
+                player_current = "O"
+            else:
+                player_current = "X"
+            number_of_tiles -= 1
     else:
         print("This tile is already taken")
+    if number_of_tiles == 0:
+        label['text'] = "Draw"
 
-# variables that set the size of the game and of the buttons, and a variable that sets how many tiles are needed to win, and a variable that tells how many free tiles there are
+# function that creates a settings window
+def settings_window():
+    pass
+
+
+
+# variables
+
+# size of the tiles
 pixelsize = 100
+# size of the board
 size = 3
-winning_tiles = 3
+# number of tiles needed to win
+#winning_tiles = 3
+# number of tiles in the board
 number_of_tiles = size*size
+# list of events
+events = []
 
 # create the window variables, and the all the global variables
 windowheight = int(pixelsize * size)
@@ -52,30 +114,35 @@ for i in range(size):
 # window setings
 window = tkinter.Tk()
 window.title("Tic Tac Toe")
-window.geometry("500x500")
-window.resizable(False, False)
-#window.iconbitmap("icon.ico")
+window.geometry("1200x700")
+window.resizable(True, True)
+window.iconbitmap("icon.ico")
+
+# create the top label
+label = tkinter.Label(window, text="Players: "+ player_current +" turn", font=("Helvetica", window.winfo_height()//size//4))
+label.pack(side="top")
+
 
 # create a frame for the buttons
 frame = tkinter.Frame(window)
 frame.pack(side="top")
-
-# create the reset button
-reset_button = tkinter.Button(window, text="Reset", command=reset, width=10, height=2)
-reset_button.pack(side="bottom")
 
 # Create buttons, and place them in the window on a grid
 for i in range(size):
     for j in range(size):
         buttons[i][j] = tkinter.Button(frame, text="", width=(size*2)*3, height=size*3, command=lambda row = i, collum = j: change_ownership(row,collum))
         buttons[i][j].grid(row=i, column=j)
-        print("Button at: ", i, j)
 
-# create the top label
-label = tkinter.Label(window, text="Player"+ player_current +"turn")
-label.pack(side="top")
+# creating a frame for the special buttons
+game_buttons = tkinter.Frame(window)
+game_buttons.pack(side="bottom")
 
+# create the reset button
+reset_button = tkinter.Button(game_buttons, text="Reset", command=reset, width=int(window.winfo_width()/size/10), height=int(window.winfo_height()/size/10/4))
+reset_button.grid(row=0, column=0)
 
-
+# creating a settings menu button
+settings_button = tkinter.Button(game_buttons, text="Settings", command=lambda: settings_window(), width=int(window.winfo_width()/size/10), height=int(window.winfo_height()/size/10/4))
+settings_button.grid(row=0, column=1)
 
 window.mainloop()
